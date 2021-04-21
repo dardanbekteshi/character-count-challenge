c3e24b39980bdda17f84754cc5039eb637e7f84e
Dardan Bekteshi <dardan.bekteshi2@gmail.com>
2021-04-21 19:57:30 +0200

    solved challenge

--- challenge.py
+++ challenge.py
@@ -1,14 +1,43 @@
 from typing import List, Tuple
+from dataclasses import dataclass, astuple, field
 
 
-def print_stuff():
-    print("hello")
+@dataclass
+class Character:
+    char: str
+    count: int
+
+    def increase(self):
+        self.count += 1
+
+
+@dataclass
+class Characters:
+    items: List[Character] = field(default_factory=list)
+
+    def add(self, item: Character) -> None:
+        self.items.append(item)
+
+    def is_current(self, char: str) -> bool:
+        return self.items[-1].char == char
+
+    def increase_current(self) -> None:
+        return self.items[-1].increase()
+
+    def __iter__(self):
+        return iter(self.items)
 
 
 def count_chars_in_order(input_string: str) -> List[Tuple[str, int]]:
-    return [
-        ("a", 4),
-        ("b", 3),
-        ("c", 2),
-        ("a", 4)
-    ]
+    characters = Characters()
+    first, *rest = input_string
+    characters.add(Character(first, 1))
+
+    for char in rest:
+        print(f"processing :{char}")
+        if characters.is_current(char):
+            characters.increase_current()
+        else:
+            characters.add(Character(char, 1))
+
+    return [astuple(item) for item in characters]
--- test_challenge.py
+++ test_challenge.py
@@ -1,18 +1,45 @@
 import challenge
 
 
-def test_print():
-    challenge.print_stuff()
-    assert True
-
-
 def test_end_2_end():
     test_input = "aaaabbbcca"
     expected = [
         ("a", 4),
         ("b", 3),
         ("c", 2),
+        ("a", 1)
+    ]
+    actual = challenge.count_chars_in_order(test_input)
+    print(actual)
+    assert actual == expected
+
+    test_input = "aaaabbbccaacccbbbdd"
+    expected = [
+        ("a", 4),
+        ("b", 3),
+        ("c", 2),
+        ("a", 2),
+        ("c", 3),
+        ("b", 3),
+        ("d", 2)
+    ]
+    actual = challenge.count_chars_in_order(test_input)
+    print(actual)
+    assert actual == expected
+
+
+def test_single_char():
+    test_input = "aaaa"
+    expected = [
         ("a", 4)
     ]
     actual = challenge.count_chars_in_order(test_input)
     assert actual == expected
+
+
+def test_increase():
+    item = challenge.Character("a", 0)
+    item.increase()
+    assert item.count == 1
+    item.increase()
+    assert item.count == 2
